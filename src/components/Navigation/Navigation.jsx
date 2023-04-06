5bef9c95fcc97e1b9316789e146ff0b9b2cb57b0
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-04-06 20:14:54 +0400

    completed navbars styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,3 +1,20 @@
-export function Navigation() {
-    return <div>Navigation</div>
+import { NavLink } from 'react-router-dom'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+export const Navigation = () => {
+    return (
+        <Navbar bg='primary' variant='dark' className='mb-3 px-5 '>
+            <Nav>
+                <Nav.Link to='/' as={NavLink}>
+                    Home
+                </Nav.Link>
+                <Nav.Link to='users' as={NavLink}>
+                    Users
+                </Nav.Link>
+                <Nav.Link to='products' as={NavLink}>
+                    Products
+                </Nav.Link>
+            </Nav>
+        </Navbar>
+    )
 }
